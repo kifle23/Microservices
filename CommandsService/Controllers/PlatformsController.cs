7223135c24f614b0afae2a086ca7399a8be19a70
kifle23 <kiflekifle23@gmail.com>
2023-12-27 13:31:07 +0300

    Create platforms controller to check async communication

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [ApiController]
+    [Route("api/c/[controller]")]
+    public class PlatformsController() : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST # Command Service");
+
+            return Ok("Inbound test of from Platforms Controler");
+        }
+    }
+}
