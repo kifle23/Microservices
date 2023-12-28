a087d9bdd34f64a0d7d54826b2bf407f2162cc55
kifle23 <kiflekifle23@gmail.com>
2023-12-28 18:42:24 +0300

    Create model Commands

---
+++ CommandsService/Models/Command.cs
@@ -0,0 +1,15 @@
+namespace CommandsService.Models
+{
+    public class Command
+    {
+        public int Id { get; set; }
+
+        public required string HowTo { get; set; }
+
+        public required string CommandLine { get; set; }
+
+        public required int PlatformId { get; set; }
+
+        public Platform? Platform { get; set; }
+    }
+}
