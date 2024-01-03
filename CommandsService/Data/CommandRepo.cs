d36667abd11fd3af4ac786b588b37b35c0e9c86c
kifle23 <kiflekifle23@gmail.com>
2024-01-03 12:18:02 +0300

    Implement Command Repo

---
+++ CommandsService/Data/CommandRepo.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using System.Linq;
+using CommandsService.Models;
+
+namespace CommandsService.Data
+{
+    public class CommandRepo(AppDbContext context) : ICommandRepo
+    {
+        private readonly AppDbContext _context = context;
+        public void CreateCommand(int platformId, Command command)
+        {
+            if (command == null)
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+
+            command.PlatformId = platformId;
+            _context.Commands.Add(command);
+        }
+
+        public void CreatePlatform(Platform plat)
+        {
+            if (plat == null)
+            {
+                throw new ArgumentNullException(nameof(plat));
+            }
+            _context.Platforms.Add(plat);
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _context.Platforms.ToList();
+        }
+
+        public Command GetCommand(int platformId, int commandId)
+        {
+            return _context.Commands
+                .FirstOrDefault(c => c.PlatformId == platformId && c.Id == commandId)
+                ?? throw new ArgumentNullException(nameof(platformId));
+        }
+
+        public IEnumerable<Command> GetCommandsForPlatform(int platformId)
+        {
+            return _context.Commands
+                .Where(c => c.PlatformId == platformId)
+                .OrderBy(c => c.Platform != null ? c.Platform.Name : null);
+        }
+
+        public bool PlaformExits(int platformId)
+        {
+            return _context.Platforms.Any(p => p.Id == platformId);
+        }
+
+        public bool SaveChanges()
+        {
+            return _context.SaveChanges() >= 0;
+        }
+    }
+}
