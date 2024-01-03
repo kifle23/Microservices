13e042691482bcfbe806f659733f96b393924060
kifle23 <kiflekifle23@gmail.com>
2024-01-03 12:31:30 +0300

    Create Command Create Dto

---
+++ CommandsService/Dtos/CommandCreateDto.cs
@@ -0,0 +1,10 @@
+
+namespace CommandsService.Dtos
+{
+    public class CommandCreateDto
+    {
+        public required string HowTo { get; set; }
+
+        public required string CommandLine { get; set; }
+    }
+}
