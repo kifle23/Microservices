310020cd687286d72d893dbbe1f3cab78f3ea8ca
kifle23 <kiflekifle23@gmail.com>
2024-01-04 12:57:58 +0300

    Update PlatformsProfile

--- PlatformService/Profiles/PlatformsProfile.cs
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -6,13 +6,14 @@ using PlatformService.Models;
 
 namespace PlatformService.Profiles
 {
-    public class PlatformsProfile: Profile
+    public class PlatformsProfile : Profile
     {
         public PlatformsProfile()
         {
             // Source -> Target
             CreateMap<Platform, PlatformReadDto>();
-            CreateMap<PlatformCreateDto, Platform>();           
+            CreateMap<PlatformCreateDto, Platform>();
+            CreateMap<PlatformReadDto, PlatformPublishedDto>();
         }
     }
 }
